fc12f8c72d645db0e4824475f1f6ccf1db8578c4
Muhammadjonov-Ahrorbek <ahrorbekmuhammadjonov653@gmail.com>
2024-07-11 21:21:33 +0500

    'dars-9'

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,36 @@
-from django.urls import path
+"""
+URL configuration for blog project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+from .views import LikeAPIView, BlogAPIView, CommentAPIView, LikeUpdateAPIView, BlogUpdateAPIView, CommentUpdateAPIView
+
 
 urlpatterns = [
-    
+    path('api/v1/like/', LikeAPIView.as_view()),
+    path('api/v1/like/<int:pk>/', LikeUpdateAPIView.as_view()),
+    path('api/v1/blog/', BlogAPIView.as_view()),
+    path('api/v1/blog/<int:pk>/', BlogUpdateAPIView.as_view()),
+    path('api/v1/comment/', CommentAPIView.as_view()),
+    path('api/v1/like/<int:pk>/', CommentUpdateAPIView.as_view()),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    path('api-auth/', include('rest_framework.urls')),
 ]
--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,21 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
